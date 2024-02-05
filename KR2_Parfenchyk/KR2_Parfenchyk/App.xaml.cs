3f7819f4ff4245badb4eaf2b10c55849383bc465
ИС212-1 <IS212-1@tisbi.ru>
2024-02-05 12:10:13 +0300

    отчисление

---
+++ KR2_Parfenchyk/KR2_Parfenchyk/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using KR2_Parfenchyk.models;
+
+
+
+
+namespace KR2_Parfenchyk
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+
+    {
+        public static KR2_ParfenchykEntities db = new KR2_ParfenchykEntities();
+        public static User loggedUser;
+
+    }
+}
